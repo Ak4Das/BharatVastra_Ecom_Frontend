60f71a5de4f42284481f4d9ec6468e6adcbde6f8
Akash Das <akashdas02052@gmail.com>
2026-07-14 16:06:33 +0530

    Change global deployed url

--- src/services/FetchRequests.js
+++ src/services/FetchRequests.js
@@ -2,7 +2,7 @@ let url = null
 if (import.meta.env.VITE_MODE === "DEVELOPMENT") {
   url = "http://localhost:3000"
 } else {
-  url = "https://bharat-vastra-ecom-backend.vercel.app/"
+  url = "https://bharat-vastra-ecom-backend.vercel.app"
 }
 
 export async function fetchCloths(obj) {
